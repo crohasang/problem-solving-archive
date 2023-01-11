1cc62ad2fe29c82b4cbb0596dd0c76b158a5571a
HS <77558413+230109@users.noreply.github.com>
2023-01-11 22:24:07 +0900

    [Bronze V] Title: 아스키 코드, Time: 208 ms, Memory: 17652 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        char a = sc.nextLine().charAt(0);
+        int b = (int)a;
+        System.out.println(b);
+    }
+
+}
