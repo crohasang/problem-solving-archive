8814ed46754ae0b044f1806fd49894e3c90a6101
HS <77558413+230109@users.noreply.github.com>
2023-04-30 02:16:38 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 292 ms, Memory: 6404 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.cc
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.cc
@@ -9,35 +9,31 @@ int cnt = 0;
 
 void DFS(int x)
 {
-    visited[x] = 1;
-    for (int i = 0; i < v[x].size(); i++)
-    {
-        int idx = v[x][i];
-        if (visited[idx] == 0)
-        {
-            DFS(idx);
-        }
-    }
+	visited[x] = 1;
+	for (int i = 0; i < v[x].size(); i++) {
+		int idx = v[x][i];
+		if (visited[idx] == 0) { // 가보지 않았다는 뜻
+			DFS(idx);
+		}
+	}
 }
 
 int main()
 {
-    int a, b;
+	int a, b;
+	cin >> N >> M;
+	for (int i = 0; i < M; i++) {
+		cin >> a >> b;
+		v[a].push_back(b);
+		v[b].push_back(a);
+	}
 
-    cin >> N >> M;
-    for (int i = 0; i < M; i++)
-    {
-        cin >> a >> b;
-        v[a].push_back(b);
-        v[b].push_back(a);
-    }
+	for (int i = 1; i <= N; i++) {
+		if (visited[i] == 0) {
+			cnt++;
+			DFS(i);
+		}
+	}
 
-    for (int i = 1; i <= N; i++){
-        if (visited[i] == 0)
-        {
-            cnt++;
-            DFS(i);
-        }
-    }
-    cout << cnt;
+	cout << cnt;
 }
