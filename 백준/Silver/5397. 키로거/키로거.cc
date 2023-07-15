35c80ce4eb18927a82b358c8960bde3fe470d671
HS <77558413+230109@users.noreply.github.com>
2023-07-15 17:42:41 +0900

    [Silver II] Title: 키로거, Time: 280 ms, Memory: 19384 KB -BaekjoonHub

--- 백준/Silver/5397. 키로거/키로거.cc
+++ 백준/Silver/5397. 키로거/키로거.cc
@@ -1,47 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(void) {
 
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
 
-  int M;
-  cin >> M;
+	int n;
 
-  for(int i=0; i<M; i++){
-  string writing;
-  cin >> writing;
-  list<char> L;
-  list<char>::iterator t = L.begin();
-  
-  for (auto a : writing) {
-    
-    if(a == '<'){
-      if(t != L.begin()){
-        t--;
-      }
-    }
-    else if(a == '>') {
-      if(t != L.end()) {
-        t++;
-      }
-    }
-    else if(a == '-') {
-      if(t != L.begin()) {
-        t--;
-        t = L.erase(t);
-      }
-    }
-    else {
-      L.insert(t, a);
-    }
-  }
+	cin >> n;
 
-  for(auto c : L) {
-    cout << c;
-  }
-  cout << '\n';
-  
+	while (n--) {
+		string x;
+		cin >> x;
 
-  }
+		list<char> a;
+		list<char>::iterator iter = a.begin();
 
+		for (int i = 0; i < x.length(); i++) {
+			if (x[i] == '<') {
+				if (iter != a.begin() && !(a.empty())) {
+					iter--;
+				}
+			}
+			else if (x[i] == '>') {
+				if (iter != a.end() && !(a.empty())) {
+					iter++;
+				}
+			}
+			else if (x[i] == '-') {
+				if (iter != a.begin()) {
+					iter--;
+					iter = a.erase(iter);
+				}
+			}
+			else {
+				a.insert(iter, x[i]);
+			}
+		}
+
+		for (auto it : a) {
+			cout << it;
+		}
+		cout << '\n';
+	}
 }
