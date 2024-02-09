f5f285181fb66b2063c549721b0a226c224e714e
HS <77558413+crohasang@users.noreply.github.com>
2024-02-10 00:17:10 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.js
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.js
@@ -1,10 +1,14 @@
 function solution(n) {
+    // 반환할 배열 초기화
     var answer = [];
+    // 배열에 값을 채울 인덱스 초기화
     var array_num = 0;
     
+    // 1부터 n까지의 홀수를 배열에 저장
     for(var i = 1; i <= n; i = i + 2) {
         answer[array_num++] = i;
     }
 
+    // 결과 배열 반환
     return answer;
 }
