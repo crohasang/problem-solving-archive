dd13104ccb8d9cefd279f4db27c206029782d24c
HS <77558413+crohasang@users.noreply.github.com>
2024-02-14 23:16:44 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.js
+++ 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.js
@@ -5,11 +5,13 @@ function solution(num_list, n) {
         let arr = [];
         
         
+        // arr에 n의 크기만큼 배열값을 넣어준다.
         for(let j = 0; j < n; j++) {
             arr[j] = num_list[i];
             i++;
         }
         
+        // answer 배열에 arr 배열을 push한다.
         answer.push(arr);
     }
     
