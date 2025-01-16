9d899bdcd9c13094b02146818550bdfffba6534a
Hasang Cho <77558413+crohasang@users.noreply.github.com>
2025-01-16 22:09:34 +0900

    [level 0] Title: 0 떼기, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.js
@@ -0,0 +1,17 @@
+function solution(n_str) {
+    let answer = '';
+    
+    let flag = 0;
+    for(let i = 0; i < n_str.length; i++) {
+        
+        if(n_str[i] !== '0') {
+            flag = 1;
+        }
+        
+        if(flag === 1) {
+            answer += n_str[i];
+        }
+    }
+    
+    return answer;
+}
