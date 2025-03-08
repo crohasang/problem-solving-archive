11107e17ff698b8a5d95ed027a17076a629407b0
Hasang Cho <77558413+crohasang@users.noreply.github.com>
2025-03-08 22:05:20 +0900

    [level 2] Title: 주식가격, Time: 5.79 ms, Memory: 45.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -0,0 +1,20 @@
+function solution(prices) {
+    const answer = [];
+    
+    for(let i = 0; i < prices.length; i++ ) {
+        let count = 0;
+    
+        for(let j = i + 1; j < prices.length; j++) {
+            if(prices[j] >= prices[i]) {
+                count++;
+            }
+            else {
+                count++;
+                break;
+            }
+        }
+        
+        answer.push(count);
+    }
+    return answer;
+}
