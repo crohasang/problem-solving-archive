fc34b657aea0848b76f8c0e2a9f04e9696bf608e
Hasang Cho <77558413+crohasang@users.noreply.github.com>
2025-09-19 22:57:42 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.46 ms, Memory: 38.6 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=javascript) 
 
 ### 성능 요약
 
-메모리: 38.4 MB, 시간: 9.03 ms
+메모리: 38.6 MB, 시간: 9.46 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 28일 17:29:28
+2025년 09월 19일 22:57:36
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,37 +1,43 @@
 function solution(maps) {
-    const queue = [];
+    const q = [];
     
-    const visitedArray = Array.from({length: maps.length}, () => new Array(maps[0].length).fill(false));
-    
-    const dist = Array.from({length: maps.length}, () => new Array(maps[0].length).fill(0));
-        
     const dx = [-1,0,1,0];
     const dy = [0,1,0,-1];
     
-    queue.push([0,0]);
-    visitedArray[0][0] = true;
+    const visited = Array.from({
+        length: maps.length}, () => new Array(maps[0].length).fill(false));
+    
+    const dist = Array.from({length: maps.length}, () => new Array(maps[0].length).fill(0));
+    
+    q.push([0,0]);
+    visited[0][0] = true;
     dist[0][0] = 1;
     
-    while(queue.length) {
-        const [curX, curY] = queue.shift();
+    let head = 0;
+    while(head < q.length) {
+        const [curX, curY] = q[head++];
         
         for(let i = 0; i < 4; i++) {
             const x = curX + dx[i];
             const y = curY + dy[i];
             
-            if(x > -1 && x < maps.length &&
-               y > -1 && y < maps[0].length) {
-                if(maps[x][y] === 1 && !visitedArray[x][y]) {
-                    queue.push([x,y]);
-                    visitedArray[x][y] = true;
+            if (x > -1 && 
+                x < maps.length &&
+                y > -1 &&
+                y < maps[0].length) {
+                
+                if(maps[x][y] === 1 & !visited[x][y]) {
+                    q.push([x,y]);
+                    visited[x][y] = true;
                     
                     if(dist[x][y] === 0) {
                         dist[x][y] = dist[curX][curY] + 1;
                     }
                 }
             }
+                
         }
     }
     
-    return dist[maps.length - 1][maps[0].length - 1] ? dist[maps.length - 1][maps[0].length - 1] : -1
+    return dist[maps.length-1][maps[0].length - 1] ? dist[maps.length-1][maps[0].length - 1 ]: -1
 }
