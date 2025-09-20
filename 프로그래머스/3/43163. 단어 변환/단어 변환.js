dcbd0fecc676fc0c7a2741f92b10f41166711197
Hasang Cho <77558413+crohasang@users.noreply.github.com>
2025-09-20 19:34:51 +0900

    [level 3] Title: 단어 변환, Time: 21.09 ms, Memory: 36.4 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.js
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.js
@@ -1,36 +1,39 @@
 function solution(begin, target, words) {
-    let answer = 0;
-    let visitedArray = Array.from({length: words.length}, () => false);
-    
-    function dfs(cur, target, step) {
-        if(cur === target) {
-            if(answer === 0 || answer > step) {
-                answer = step;
-            }
-            return;
-        }
-        
-        for(let i = 0; i < words.length; i++) {
-            if(visitedArray[i]) {
-                continue;
-            }
-            
-            let differentCount = 0;
-            
-            for(let j = 0; j < words[i].length; j++) {
-                if(words[i][j] !== cur[j]) {
-                    differentCount += 1;
-                }
-            }
-            
-            if (differentCount === 1) {
-                visitedArray[i] = true;
-                dfs(words[i], target, step + 1);
-                visitedArray[i] = false;
-            }
-        }
+    if(!words.includes(target)) {
+        return 0;
     }
     
-    dfs(begin, target, 0);
+    let answer = 0;
+    const visited = new Set();
+    let q = [begin];    
+    
+    while(q.length) {
+        let temp = [];
+        
+        for (const word of q) {
+            visited.add(word);
+                    
+            if(word === target) {
+                return answer;
+            }
+            
+            
+            for (let i = 0; i < word.length; i++) {
+                const letter = slicedWord(word, i);
+                const matched = words.filter((v, j) => !visited.has(v) && slicedWord(v,i) === letter);
+                temp.push(...matched);
+            }
+        }
+        
+        answer++;
+        q = temp;
+    }
+   
+    function slicedWord(word, i) {
+        const wordToArray = word.split('');
+        wordToArray.splice(i, 1);
+        return wordToArray.join('');   
+    }
+    
     return answer;
 }
