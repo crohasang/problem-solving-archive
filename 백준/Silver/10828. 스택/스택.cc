380846abcdb31e179ebc05dc3adc185f53b3df2a
Hasang Cho <77558413+crohasang@users.noreply.github.com>
2025-10-11 22:18:27 +0900

    [Silver IV] Title: 스택, Time: 212 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.cc
+++ 백준/Silver/10828. 스택/스택.cc
@@ -1,48 +1,49 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <stack>
+
 using namespace std;
 
+int main() {
+    stack<int> stack;
+    int N;
+    cin >> N;
 
-int main(void) {
-  int N;
-  cin >> N;
-  stack<int> S;
-  for(int i=0; i < N; i++){
-    string a;
-    cin >> a;
-    if(a == "push"){
-      int X;
-      cin >> X;
-      S.push(X);
-    }
-    else if (a == "pop"){
-      if(S.empty() == true){
-        cout << -1 << "\n";
-      }
-      else {
-        cout << S.top() << "\n";
-        S.pop();
-      }
-  
-    }
-    else if (a == "size"){
-      cout << S.size() << "\n";
-    }
-    else if (a == "empty"){
-      if(S.empty() == true){
-        cout << 1 << "\n";
-      }
-      else {
-        cout << 0 << "\n";
-      }
+    for(int i = 0; i < N; i++) {
+        string command;
+        cin >> command;
 
+        if (command == "push") {
+            int push_number;
+            cin >> push_number;
+            stack.push(push_number);
+        }
+        else if (command == "top") {
+            if(stack.empty()) {
+                cout << "-1" << "\n";
+            }
+            else {
+                cout << stack.top() << "\n";
+            }
+        }
+        else if (command == "size") {
+            cout << stack.size() << "\n";
+        }
+        else if (command == "empty") {
+            if(stack.empty()) {
+                cout << "1" << "\n";
+            }
+            else {
+                cout << "0" << "\n";
+            }
+        }
+        else if (command == "pop") {
+            if (stack.empty()) {
+                cout << "-1" << "\n";
+            }
+            else {
+                cout << stack.top() << "\n";
+                stack.pop();
+            }
+        }
     }
-    else if (a == "top") {
-      if(S.empty() == true){
-        cout << -1 << "\n";
-      }
-      else {
-        cout << S.top() << "\n";
-      }
-    }
-  }
 }
