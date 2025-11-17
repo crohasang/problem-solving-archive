19572ee6df1af3d3a03fb3418bb520bcc0f2df73
Hasang Cho <77558413+crohasang@users.noreply.github.com>
2025-11-18 01:02:04 +0900

    [Silver V] Title: K번째 수, Time: 1008 ms, Memory: 21432 KB -BaekjoonHub

---
+++ 백준/Silver/11004. K번째 수/K번째 수.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+
+
+using namespace std;
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int N, K;
+    cin >> N >> K;
+    int arr[N];
+
+    
+    
+    for(int i = 0; i < N; i++) {
+        int x;
+        cin >> x;
+        arr[i] = x;
+    };
+
+    sort(arr, arr + N);
+
+
+    cout << arr[K-1];
+    
+
+}
