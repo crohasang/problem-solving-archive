d83e29b7776c2e90c003ddc163be4ae453cace74
Hasang Cho <77558413+crohasang@users.noreply.github.com>
2026-01-11 15:32:04 +0900

    [Silver III] Title: 파도반 수열, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.py
@@ -0,0 +1,22 @@
+'''
+1 1 1 2 2 3 4 5 7 9
+
+방향?
+
+'''
+
+
+import sys
+
+
+arr = [0] * 101
+arr[1], arr[2], arr[3] = 1, 1, 1
+
+for i in range(4, 101):
+    arr[i] = arr[i-2] + arr[i-3]
+
+T = int(sys.stdin.readline())
+
+for i in range(T):
+    N = int(sys.stdin.readline())
+    print(arr[N])
